9ab9d1d409ed678ca46b4039d7ebbff4de377e62
Loki <david@dusk.network>
2021-10-09 07:56:43 +0300

    disabling ipc test to get correct

--- bls/ipc_test.go
+++ bls/ipc_test.go
@@ -1,45 +1,42 @@
 package bls
 
-import (
-	"testing"
-)
-
-func TestGenerateKeysIPC(t *testing.T) {
-	SwitchToIPC()
-	TestGenerateKeysIPC(t)
-}
-
-func TestSignVerifyIPC(t *testing.T) {
-	SwitchToIPC()
-	TestSignVerifyIPC(t)
-}
-
-func TestVerifyWrongKeyIPC(t *testing.T) {
-	SwitchToIPC()
-	TestVerifyWrongKeyIPC(t)
-}
-
-func TestAggregationIPC(t *testing.T) {
-	SwitchToIPC()
-	TestAggregationIPC(t)
-}
-
-func BenchmarkSignIPC(b *testing.B) {
-	SwitchToIPC()
-	BenchmarkSignIPC(b)
-}
-
-func BenchmarkVerifyIPC(b *testing.B) {
-	SwitchToIPC()
-	BenchmarkVerifyIPC(b)
-}
-
-func BenchmarkAggregatePkIPC(b *testing.B) {
-	SwitchToIPC()
-	BenchmarkAggregatePkIPC(b)
-}
-
-func BenchmarkAggregateSigIPC(b *testing.B) {
-	SwitchToIPC()
-	BenchmarkAggregateSigIPC(b)
-}
+//
+// func TestGenerateKeysIPC(t *testing.T) {
+// 	SwitchToIPC()
+// 	TestGenerateKeysIPC(t)
+// }
+//
+// func TestSignVerifyIPC(t *testing.T) {
+// 	SwitchToIPC()
+// 	TestSignVerifyIPC(t)
+// }
+//
+// func TestVerifyWrongKeyIPC(t *testing.T) {
+// 	SwitchToIPC()
+// 	TestVerifyWrongKeyIPC(t)
+// }
+//
+// func TestAggregationIPC(t *testing.T) {
+// 	SwitchToIPC()
+// 	TestAggregationIPC(t)
+// }
+//
+// func BenchmarkSignIPC(b *testing.B) {
+// 	SwitchToIPC()
+// 	BenchmarkSignIPC(b)
+// }
+//
+// func BenchmarkVerifyIPC(b *testing.B) {
+// 	SwitchToIPC()
+// 	BenchmarkVerifyIPC(b)
+// }
+//
+// func BenchmarkAggregatePkIPC(b *testing.B) {
+// 	SwitchToIPC()
+// 	BenchmarkAggregatePkIPC(b)
+// }
+//
+// func BenchmarkAggregateSigIPC(b *testing.B) {
+// 	SwitchToIPC()
+// 	BenchmarkAggregateSigIPC(b)
+// }
